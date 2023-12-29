a18568c4d9b1831bfcf0fd75c8ff13ac6c8b8392
Malav_P <malavpatel2022@u.northwestern.edu>
2023-12-29 14:17:53 -0600

    R_inv parameter tweaks

--- src/SensorTasking/observation_model.py
+++ src/SensorTasking/observation_model.py
@@ -9,8 +9,8 @@ def some_function(truth, observers):
     Z = np.zeros(shape=(dim, num_observers))
 
     for i in range(3):
-        R_invs[i, i, :] = 1/0.001   # positional uncertaintiy of +- 384 km (VERY CONSERVATIVE, most sensor can do better)
-        R_invs[3 + i, 3 + i, :] = 1/0.01 # velocity uncertainty ~ +- 10 m/s
+        R_invs[i, i, :] = 1/(0.001**2)   # positional uncertaintiy of +- 384 km (VERY CONSERVATIVE, most sensor can do better)
+        R_invs[3 + i, 3 + i, :] = 1/(0.01**2) # velocity uncertainty ~ +- 0.01 km/s
 
     Z_transpose = np.array([truth.x + np.random.multivariate_normal(mean=np.zeros(dim), cov=np.linalg.inv(R_invs[:,:,j])) for j in range(num_observers)])
     Z = Z_transpose.T
